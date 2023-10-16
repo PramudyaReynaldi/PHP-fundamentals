4f3d04b2a7c1e13a4bfbe7a35d28af48684b857c
PramudyaReynaldi <prmdy02@gmail.com>
2023-10-16 21:48:58 +0700

    learn variable

---
+++ variable/variable.php
@@ -0,0 +1,25 @@
+<?php
+
+// --------------------------------------------------
+// |                  VARIABLE                      |
+// |                                                |
+// |     Untuk menyimpan sebuah nilai/data          |
+// |                                                |
+// --------------------------------------------------
+
+// Menampung data string
+
+$name = "John"; // variabel name menyimpan sebuah nilai berupa string
+$age = 20; // variabel age menyimpan sebuah nilai berupa number yaitu integer
+
+// mencetak variabel name dan age
+
+echo "Name : ";
+echo $name;
+
+echo "\n";
+
+echo "Age : ";
+echo $age;
+
+echo "\n";
